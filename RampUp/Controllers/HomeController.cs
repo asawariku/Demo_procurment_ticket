c47effe61c266cacb403dca761c9234b63159a9e
Asawari Abhay Kulkarni <asawariku@cybage.com>
2022-02-09 06:53:03 +0000

    merging 2
    git-tfs-id: [http://tcvc103/ALM_TFS17]$/asawariku/RampUpBranch/FeatureBranch;C467

--- RampUp/Controllers/HomeController.cs
+++ RampUp/Controllers/HomeController.cs
@@ -17,6 +17,7 @@ namespace RampUp.Controllers
     //Suspend and Resume
     //change 4
     //merging
+    //merging 2
     public class HomeController : Controller
     {
         public ActionResult Index()
