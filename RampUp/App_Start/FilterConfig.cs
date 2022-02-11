9d3aa738374820bea31cc04239a259430121a9ef
Asawari Abhay Kulkarni <asawariku@cybage.com>
2022-02-11 06:40:00 +0000

    suppression
    git-tfs-id: [http://tcvc103/ALM_TFS17]$/asawariku/RampUp;C497

--- RampUp/App_Start/FilterConfig.cs
+++ RampUp/App_Start/FilterConfig.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 
 namespace RampUp
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1053:StaticHolderTypesShouldNotHaveConstructors")]
     public class FilterConfig
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
